46b3be923792960e35aff48670313bf5988c37c7
axmedjaa <axmedja44@gmail.com>
2025-06-20 10:52:03 +0300

    updated readmi

--- utils/swagger.js
+++ utils/swagger.js
@@ -1,5 +1,6 @@
 import swaggerJSDoc from 'swagger-jsdoc';
-
+import dotenv from 'dotenv'
+dotenv.config()
 const options = {
   definition: {
     openapi: '3.0.0',
@@ -10,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000'
+        url: process.env.NODE_ENV=="development"?'http://localhost:3000':'https://montorship-api.onrender.com'
       }
     ],
     components: {
